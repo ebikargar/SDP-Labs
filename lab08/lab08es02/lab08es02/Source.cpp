4f30abecfc7103a41dae63e34007836ce3390118
MartinoMensio <martinomensio@outlook.it>
2016-05-04 16:32:52 +0200

    lab08es02 checking file with output on console

--- lab08/lab08es02/lab08es02/Source.cpp
+++ lab08/lab08es02/lab08es02/Source.cpp
@@ -32,14 +32,14 @@ INT _tmain(INT argc, LPTSTR argv[]) {
 	fInP = _tfopen(argv[1], _T("rt"));
 
 	if (fInP == NULL) {
-		_ftprintf(stderr, _T("Cannot open input file. Error: %x\n"), GetLastError());
+		_ftprintf(stderr, _T("Cannot open input file %s. Error: %x\n"), argv[1], GetLastError());
 		return 2;
 	}
 
 	hOut = CreateFile(argv[2], GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	if (hOut == INVALID_HANDLE_VALUE) {
-		_ftprintf(stderr, _T("Cannot open output file. Error: %x\n"), GetLastError());
+		_ftprintf(stderr, _T("Cannot open output file %s. Error: %x\n"), argv[2], GetLastError());
 		fclose(fInP);
 		return 3;
 	}
@@ -56,5 +56,21 @@ INT _tmain(INT argc, LPTSTR argv[]) {
 	_tprintf(_T("Correctly written the output file\n"));
 	fclose(fInP);
 	CloseHandle(hOut);
+
+	_tprintf(_T("Now reading it back:\n"));
+	HANDLE hIn;
+	DWORD nIn;
+	hIn = CreateFile(argv[2], GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hIn == INVALID_HANDLE_VALUE) {
+		_ftprintf(stderr, _T("Cannot open the file %s. Error: %x\n"), argv[2], GetLastError());
+		return 5;
+	}
+	while (ReadFile(hIn, &s, sizeof(s), &nIn, NULL) && nIn > 0) {
+		if (nIn != sizeof(s)) {
+			_ftprintf(stderr, _T("Error reading, file shorter than expected\n"));
+		}
+		_tprintf(_T("id: %d\treg_number: %ld\tsurname: %10s\tname: %10s\tmark: %d\n"), s.id, s.regNum, s.surname, s.name, s.mark);
+	}
+	CloseHandle(hIn);
 	return 0;
 }
