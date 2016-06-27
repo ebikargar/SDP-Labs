3fd09aa7fb98c2c4e9dd74dab1591691c4d52b1f
MartinoMensio <martinomensio@outlook.it>
2016-06-27 15:58:57 +0200

    lab13es01 crazy stuff (need to simplify output records)

--- lab13/lab13es01/lab13es01/lab13es01.cpp
+++ lab13/lab13es01/lab13es01/lab13es01.cpp
@@ -26,6 +26,9 @@ TODO description
 #define PREFIX_NAME_LEN 3
 #define PREFIX_NAME_SEM _T("SEM")
 #define PREFIX_NAME_MUT _T("MUT")
+#define MAX_OUTPUT_FILES 128
+#define SWAP_TIMEOUT 50		// time to change part of WaitForMultipleObjects with more than 64 objects
+#define TERMINATION_SEMAPHORE_NAME _T("TerminationSemaphore")
 
 typedef struct _record {
 	TCHAR directoryName[NAME_MAX_LEN];
@@ -56,8 +59,9 @@ typedef struct _processingParam {
 
 typedef struct _collectorParam {
 	LPTSTR filename;
-	INT updateFileInterval;
+	DWORD updateFileInterval;
 	LPPROTECTEDBOOL pb;
+	DWORD nProcessing;	// collector needs to know how many processing threads
 } COLLECTORPARAM, *LPCOLLECTORPARAM;
 
 BOOL rotateBackslashes(LPTSTR stringToManipulate);
@@ -72,7 +76,7 @@ VOID protectedBoolSet(LPPROTECTEDBOOL pb, BOOL value);
 BOOL protectedBoolGet(LPPROTECTEDBOOL pb);
 VOID protectedBoolClean(LPPROTECTEDBOOL pb);
 
-BOOL WaitForVeryHugeNumberOfObjects(LPHANDLE handles, DWORD count);
+DWORD WaitForVeryHugeNumberOfObjects(LPHANDLE handles, DWORD count, BOOL waitForAll);
 
 INT _tmain(INT argc, LPTSTR argv[]) {
 	HANDLE hIn;
@@ -136,6 +140,7 @@ INT _tmain(INT argc, LPTSTR argv[]) {
 	cp.filename = argv[1];
 	cp.updateFileInterval = M;
 	cp.pb = &pb;
+	cp.nProcessing = N;
 
 	// create last thread
 	hThreads[N] = CreateThread(NULL, 0, collectorThreadFunction, &cp, 0, NULL);
@@ -150,7 +155,7 @@ INT _tmain(INT argc, LPTSTR argv[]) {
 	}
 	
 	// wait all the threads
-	if (!WaitForVeryHugeNumberOfObjects(hThreads, N)) {
+	if (WaitForVeryHugeNumberOfObjects(hThreads, N, TRUE) == WAIT_FAILED) {
 		_ftprintf(stderr, _T("Impossible to wait for all the threads\n"));
 		for (i = 0; i < N + 1; i++) {
 			TerminateThread(hThreads[i], 1);
@@ -205,8 +210,15 @@ DWORD WINAPI processingThreadFunction(LPVOID p) {
 	HANDLE hMutexOutput, hSemaphoreOutput, hFileOutput;
 	TCHAR mutexOutputName[NAME_MAX_LEN + PREFIX_NAME_LEN];
 	TCHAR semaphoreOutputName[NAME_MAX_LEN + PREFIX_NAME_LEN];
+	HANDLE terminationSemaphore;
+	OUTPUTRECORD outputRecord;
 
 	//_tprintf(_T("I am alive\n"));
+	terminationSemaphore = CreateSemaphore(NULL, 0, INFINITE, TERMINATION_SEMAPHORE_NAME);
+	if (terminationSemaphore == INVALID_HANDLE_VALUE) {
+		_ftprintf(stderr, _T("Impossible to open the termination semaphore. Error: %x\n"), GetLastError());
+		return 1;
+	}
 	
 	while (TRUE) {
 		EnterCriticalSection(&pp->cs);
@@ -214,12 +226,17 @@ DWORD WINAPI processingThreadFunction(LPVOID p) {
 		LeaveCriticalSection(&pp->cs);
 		if (result == FALSE) {
 			_ftprintf(stderr, _T("Impossible to read a record from the input file. Error: %x\n"), GetLastError());
-			return 2;
+			return 1;
 		}
 		if (nRead == 0) {
 			// end of file
 			Sleep(10000);
-			protectedBoolSet(pp->pb, TRUE);
+			// TODO signal on termination semaphore
+			if (!ReleaseSemaphore(terminationSemaphore, 1, NULL)) {
+				_ftprintf(stderr, _T("Impossible to release termination semaphore. Error: %x\n"), GetLastError());
+				return 2;
+			}
+			//protectedBoolSet(pp->pb, TRUE);
 			break;
 		}
 		if (nRead != sizeof(RECORD)) {
@@ -234,6 +251,14 @@ DWORD WINAPI processingThreadFunction(LPVOID p) {
 		scan directory
 		for each file, process it (counting chars and newlines) and add to the outputRecord (may need realloc)
 		*/
+		outputRecord.nFiles = 3;
+		outputRecord.filesInfo = (LPFILEINFOCOLLECTED)calloc(outputRecord.nFiles, sizeof(FILEINFOCOLLECTED));
+		outputRecord.filesInfo[0].nChar = 1;
+		outputRecord.filesInfo[0].nNewLines = 2;
+		outputRecord.filesInfo[1].nChar = 3;
+		outputRecord.filesInfo[1].nNewLines = 4;
+		outputRecord.filesInfo[2].nChar = 5;
+		outputRecord.filesInfo[2].nNewLines = 6;
 
 
 		_tcsncpy(semaphoreOutputName, PREFIX_NAME_SEM, NAME_MAX_LEN);
@@ -277,10 +302,17 @@ DWORD WINAPI processingThreadFunction(LPVOID p) {
 			return 8;
 		}
 		// write
-		WriteFile(hFileOutput, _T("ciao"), 5 * sizeof(TCHAR), &nWritten, NULL);
+		//WriteFile(hFileOutput, _T("ciao"), 5 * sizeof(TCHAR), &nWritten, NULL);
+		if (!outputRecordWrite(hFileOutput, &outputRecord)) {
+			_ftprintf(stderr, _T("Impossible to write output record. Error: %x\n"), GetLastError());
+			return 9;
+		}
 		CloseHandle(hFileOutput);
 		// release the mutex for the file
 		ReleaseMutex(hMutexOutput);
+
+		// Signal that a record has been written on the output file
+		ReleaseSemaphore(hSemaphoreOutput, 1, NULL);
 	}
 
 	return 0;
@@ -288,9 +320,105 @@ DWORD WINAPI processingThreadFunction(LPVOID p) {
 
 DWORD WINAPI collectorThreadFunction(LPVOID p) {
 	LPCOLLECTORPARAM cp = (LPCOLLECTORPARAM)p;
+	HANDLE hRecordsFile;
+	//HANDLE hCurrentOutputFile;
+	RECORD record;
+	DWORD nRead, nOutputFiles, i;
+	LONG found;
+	TCHAR outputFileNames[MAX_OUTPUT_FILES][NAME_MAX_LEN];
+	TCHAR semaphoreOutputName[NAME_MAX_LEN + PREFIX_NAME_LEN];
+	HANDLE hSemaphoresOutputAndTerminationSemaphore[MAX_OUTPUT_FILES + 1];
+	DWORD nTerminated;
+	DWORD rowsReady[MAX_OUTPUT_FILES];
+	HANDLE hOutputFiles[MAX_OUTPUT_FILES];
+	DWORD result;
+	OUTPUTRECORD outRecord;
+	memset(outputFileNames, 0, sizeof(outputFileNames));
 
-	while (!protectedBoolGet(cp->pb)) {
-		Sleep(5000);
+	hRecordsFile = CreateFile(cp->filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hRecordsFile == INVALID_HANDLE_VALUE) {
+		_ftprintf(stderr, _T("Impossible to open the records file. Error: %x\n"), GetLastError());
+		return 2;
+	}
+	// read the file to get the output file names
+	nOutputFiles = 0;
+	while (TRUE) {
+		if (!ReadFile(hRecordsFile, &record, sizeof(RECORD), &nRead, NULL)) {
+			_ftprintf(stderr, _T("Impossible to read from records file. Error: %x\n"), GetLastError());
+			CloseHandle(hRecordsFile);
+			return 3;
+		}
+		found = -1;
+		for (i = 0; i < nOutputFiles; i++) {
+			if (_tcsncmp(outputFileNames[i], record.outputName, NAME_MAX_LEN) == 0) {
+				found = i;
+				break;
+			}
+		}
+		if (found == -1) {
+			if (nOutputFiles >= MAX_OUTPUT_FILES) {
+				_ftprintf(stderr, _T("Too many records in this file\n"));
+				CloseHandle(hRecordsFile);
+				return 4;
+			}
+			_tcsncpy(outputFileNames[i], record.outputName, NAME_MAX_LEN);
+			nOutputFiles++;
+		}
+	}
+	CloseHandle(hRecordsFile);
+
+	// Now open all the semaphores for the output files
+	
+	for (i = 0; i < nOutputFiles; i++) {
+		_tcsncpy(semaphoreOutputName, PREFIX_NAME_SEM, NAME_MAX_LEN);
+		_tcsncat(semaphoreOutputName, outputFileNames[i], NAME_MAX_LEN - PREFIX_NAME_LEN);
+		rotateBackslashes(semaphoreOutputName);
+
+		hSemaphoresOutputAndTerminationSemaphore[i] = CreateSemaphore(NULL, 0, INFINITE, semaphoreOutputName);
+		if (hSemaphoresOutputAndTerminationSemaphore[i] == INVALID_HANDLE_VALUE) {
+			_ftprintf(stderr, _T("Impossible to open semaphore for output file (collector). Error: %x\n"), GetLastError());
+			return 1;
+		}
+		rowsReady[i] = 0;
+		hOutputFiles[i] = CreateFile(outputFileNames[result], GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hOutputFiles[i] == INVALID_HANDLE_VALUE) {
+			_ftprintf(stderr, _T("Impossible to open output file %s. Error: %x"), outputFileNames[result], GetLastError());
+			return 2;
+		}
+	}
+	// the last handle of the array is the termination Semaphore (signalled by threads that reach end of records file
+	hSemaphoresOutputAndTerminationSemaphore[nOutputFiles] = CreateSemaphore(NULL, 0, INFINITE, TERMINATION_SEMAPHORE_NAME);
+	if (hSemaphoresOutputAndTerminationSemaphore[nOutputFiles] == INVALID_HANDLE_VALUE) {
+		_ftprintf(stderr, _T("Impossible to open termination semaphore (collector). Error: %x\n"), GetLastError());
+		return 1;
+	}
+	nTerminated = 0;
+
+	while (nTerminated < cp->nProcessing) {
+		result = WaitForVeryHugeNumberOfObjects(hSemaphoresOutputAndTerminationSemaphore, nOutputFiles + 1, FALSE);
+		if (result == WAIT_FAILED) {
+			_ftprintf(stderr, _T("Impossible to wait very huge number of objects (collector)"));
+			return 1;
+		}
+		assert(result <= nOutputFiles);
+		if (result == nOutputFiles) {
+			// a thread reached end of input file
+			nTerminated++;
+		} else {
+			// a thread wrote on some output file
+			rowsReady[result]++;
+			if (rowsReady[result] == cp->updateFileInterval) {
+				_tprintf(_T("File %s:\n"), outputFileNames[result]);
+				// time to process this file
+				for (i = 0; i < cp->updateFileInterval; i++) {
+					if (!outputRecordRead(hOutputFiles[result], &outRecord)) {
+						_ftprintf(stderr, _T("Impossible to read output record\n"));
+						return 2;
+					}
+					_tprintf(_T("\tn_files: %d\n"), outRecord.nFiles);
+				}
+			}
+		}
 	}
 	return 0;
 }
@@ -358,17 +486,54 @@ VOID protectedBoolClean(LPPROTECTEDBOOL pb) {
 	DeleteCriticalSection(&pb->cs);
 }
 
-BOOL WaitForVeryHugeNumberOfObjects(LPHANDLE handles, DWORD count) {
-	DWORD waited, toWait, toWaitNow;
+// if WaitForAll == TRUE returns 0 or WAIT_FAILED
+// if WaitForAll == FALSE returns the index of the handle ready
+// if WaitForAll == FALSE and count greater than 64, the wait is done with timeout on groups of 64 members
+DWORD WaitForVeryHugeNumberOfObjects(LPHANDLE handles, DWORD count, BOOL waitForAll) {
+	DWORD waited, toWait, toWaitNow, result, nChunks, i, chunkN;
 	toWait = count;
 	waited = 0;
-	while (toWait > 0) {
-		toWaitNow = (toWait > MAXIMUM_WAIT_OBJECTS) ? MAXIMUM_WAIT_OBJECTS : toWait;
-		if (WaitForMultipleObjects(toWaitNow, &handles[waited], TRUE, INFINITE) == WAIT_FAILED) {
-			return FALSE;
+	if (waitForAll) {
+		// wait for all the handles
+		while (toWait > 0) {
+			toWaitNow = (toWait > MAXIMUM_WAIT_OBJECTS) ? MAXIMUM_WAIT_OBJECTS : toWait;
+			if (WaitForMultipleObjects(toWaitNow, &handles[waited], TRUE, INFINITE) == WAIT_FAILED) {
+				return WAIT_FAILED;
+			}
+			waited += toWaitNow;
+			toWait -= toWaitNow;
 		}
-		waited += toWaitNow;
-		toWait -= toWaitNow;
+		return 0;
+	} else {
+		// just want to wait for one of them
+		if (count > MAXIMUM_WAIT_OBJECTS) {
+			nChunks = count / MAXIMUM_WAIT_OBJECTS;
+			// wait on chuncks with timeout
+			chunkN = 0;
+			while (TRUE) {
+				// compute on how many i will wait
+				toWaitNow = (chunkN == nChunks - 1) ? (count - MAXIMUM_WAIT_OBJECTS * chunkN) : MAXIMUM_WAIT_OBJECTS;
+				result = WaitForMultipleObjects(toWaitNow, &handles[chunkN * MAXIMUM_WAIT_OBJECTS], FALSE, SWAP_TIMEOUT);
+				if (result == WAIT_TIMEOUT) {
+					// i want to wait for the next chunk
+					chunkN = (chunkN + 1) % nChunks;
+					continue;
+				} else if (result == WAIT_FAILED) {
+					_ftprintf(stderr, _T("WaitForMultipleObjects WAIT_FAILED. Error: %x"), GetLastError());
+					return WAIT_FAILED;
+				} else {
+					result -= WAIT_OBJECT_0;
+					// compute the index of the original array of handles
+					result += chunkN * MAXIMUM_WAIT_OBJECTS;
+					return result;
+				}
+
+				
+			}
+		} else {
+			// less than WAIT_OBJECT_0 handles
+			return WaitForMultipleObjects(count, handles, FALSE, INFINITE) - WAIT_OBJECT_0;
+		}
 	}
-	return TRUE;
+	
 }
